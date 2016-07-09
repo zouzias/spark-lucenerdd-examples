83987c99a6e0f9d87b117fd61fc8b90b9752a726
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-07-09 13:58:52 +0200

    better println

--- src/main/scala/org/zouzias/spark/lucenerdd/examples/HelloSparkLuceneRDD.scala
+++ src/main/scala/org/zouzias/spark/lucenerdd/examples/HelloSparkLuceneRDD.scala
@@ -2,7 +2,6 @@ package org.zouzias.spark.lucenerdd.examples
 
 // import required spark classes
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.zouzias.spark.lucenerdd.LuceneRDD
 import org.zouzias.spark.lucenerdd._
@@ -13,11 +12,17 @@ object HelloSparkLuceneRDD {
 
     // initialise spark context
     val conf = new SparkConf().setAppName("HelloSparkLuceneRDD")
-    val sc = new SparkContext(conf)
+
+    //
+    implicit val sc = new SparkContext(conf)
 
     val array = Array("Hello", "world")
     val rdd = LuceneRDD(array)
-    rdd.count
+    val count = rdd.count
+
+    println("=" * 10)
+    println(s"Count is ${count}")
+    println("=" * 10)
 
     // terminate spark context
     sc.stop()
