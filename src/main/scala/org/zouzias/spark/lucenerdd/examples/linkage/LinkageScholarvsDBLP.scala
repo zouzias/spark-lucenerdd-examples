572d29c64a2c5a410a21fe914f765f61f5aa7b54
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-09-18 18:43:28 +0200

    linkage: fix scholar vs DBLP

--- src/main/scala/org/zouzias/spark/lucenerdd/examples/linkage/LinkageScholarvsDBLP.scala
+++ src/main/scala/org/zouzias/spark/lucenerdd/examples/linkage/LinkageScholarvsDBLP.scala
@@ -1,6 +1,6 @@
 package org.zouzias.spark.lucenerdd.examples.linkage
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.{Logging, SparkConf, SparkContext}
 import org.zouzias.spark.lucenerdd.LuceneRDD
 import org.zouzias.spark.lucenerdd._
@@ -26,12 +26,16 @@ object LinkageScholarvsDBLP extends Logging {
     logInfo(s"Loaded ${scholarDF.count} DBLP records")
     val groundTruthDF = sqlContext.read.parquet("data/linkage-papers1/linkage-papers-scholar-vs-dblp.parquet")
 
-    val scholar = scholarDF.select("id", "title", "authors", "venue").map( row => (row.get(0).toString, row.getString(1), row.getString(2)))
+    val scholar = scholarDF.select("id", "title", "authors", "venue")
+
     val dblp = LuceneRDD(dblpDF)
 
     // A custom linker
-    val linker: (String, String, String) => String = {
-      case (_, title, authors) => {
+    val linker: Row => String = {
+      case row => {
+        val title = row.getString(row.fieldIndex("title"))
+        val authors = row.getString(row.fieldIndex("authors"))
+
         val titleTokens = title.split(" ").map(_.replaceAll("[^a-zA-Z0-9]", "")).filter(_.length > 3).mkString(" OR ")
         val authorsTerms = authors.split(" ").map(_.replaceAll("[^a-zA-Z0-9]", "")).filter(_.length > 2).mkString(" OR ")
 
@@ -50,13 +54,15 @@ object LinkageScholarvsDBLP extends Logging {
       }
     }
 
-    val linkedResults = dblp.link(scholar, linker.tupled, 3)
+    val linkedResults = dblp.linkDataFrame(scholar, linker, 3)
 
     import sqlContext.implicits._
 
-    val linkageResults = linkedResults.filter(_._2.nonEmpty).map{ case (scholar, topDocs) => (topDocs.head.doc.textField("_1").head, scholar._1)}.toDF("idDBLP", "idScholar")
+    val linkageResults = linkedResults.filter(_._2.nonEmpty).map{ case (scholar, topDocs) => (topDocs.head.doc.textField("id").head, scholar.getString(scholar.fieldIndex("id")))}
+      .toDF("idDBLP", "idScholar")
 
-    val correctHits: Double = linkageResults.join(groundTruthDF, groundTruthDF.col("idDBLP").equalTo(linkageResults("idDBLP")) &&  groundTruthDF.col("idScholar").equalTo(linkageResults("idScholar"))).count
+    val correctHits: Double = linkageResults
+      .join(groundTruthDF, groundTruthDF.col("idDBLP").equalTo(linkageResults("idDBLP")) &&  groundTruthDF.col("idScholar").equalTo(linkageResults("idScholar"))).count
     val total: Double = groundTruthDF.count
     val accuracy = correctHits / total
 
