bbee9da05a7b6c0b7904434fb9e3ffe709aafc51
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-10-01 21:01:13 +0200

    branch_2.x: update submit script

--- spark-submit.sh
+++ spark-submit.sh
@@ -5,17 +5,20 @@ CURRENT_DIR=`pwd`
 # Assumes that spark is installed under home directory
 HOME_DIR=`echo ~`
 export SPARK_LOCAL_IP=localhost
-SPARK_HOME=${HOME_DIR}/spark-1.6.2-bin-hadoop2.6
+SPARK_HOME=${HOME_DIR}/spark-2.0.0-bin-hadoop2.7
 
 # spark-lucenerdd assembly JAR
-MAIN_JAR=${CURRENT_DIR}/target/scala-2.10/spark-lucenerdd-examples-assembly-0.0.24.jar
+MAIN_JAR=${CURRENT_DIR}/target/scala-2.11/spark-lucenerdd-examples-assembly-0.2.0.jar
 
 echo "Running example class $1"
 
 # Run spark shell locally
 ${SPARK_HOME}/bin/spark-submit   --conf "spark.executor.memory=512m" \
 				--conf "spark.driver.memory=512m" \
-				--master local[2] \
+				--conf "spark.rdd.compress=true" \
+				--conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
+				--conf "spark.kryoserializer.buffer=24mb" \
+				--master local[*] \
 				--class $1 \
 				--jars ${MAIN_JAR} \
 				"${MAIN_JAR}"
