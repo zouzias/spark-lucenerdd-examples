22ce2dc4ae25099c86ad0873dcfd51971c44a131
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-10-15 12:04:41 +0200

    use luceneRDD 0.2.2-SNAPSHOT

--- spark-submit.sh
+++ spark-submit.sh
@@ -8,7 +8,7 @@ export SPARK_LOCAL_IP=localhost
 SPARK_HOME=${HOME_DIR}/spark-2.0.1-bin-hadoop2.7
 
 # spark-lucenerdd assembly JAR
-MAIN_JAR=${CURRENT_DIR}/target/scala-2.11/spark-lucenerdd-examples-assembly-0.2.1.jar
+MAIN_JAR=${CURRENT_DIR}/target/scala-2.11/spark-lucenerdd-examples-assembly-0.2.2-SNAPSHOT.jar
 
 echo "Running example class $1"
 
@@ -18,6 +18,7 @@ ${SPARK_HOME}/bin/spark-submit   --conf "spark.executor.memory=512m" \
 				--conf "spark.rdd.compress=true" \
 				--conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
 				--conf "spark.kryoserializer.buffer=24mb" \
+				--conf "spark.kryo.registrator=org.zouzias.spark.lucenerdd.LuceneRDDKryoRegistrator" \
 				--master local[*] \
 				--class $1 \
 				--jars ${MAIN_JAR} \
