f5abbd30d99fd7a94a8fdbdaf19484b464b0c4a7
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-10-16 11:36:22 +0200

    scripts: increase spark memory

--- spark-submit.sh
+++ spark-submit.sh
@@ -13,8 +13,8 @@ MAIN_JAR=${CURRENT_DIR}/target/scala-2.11/spark-lucenerdd-examples-assembly-0.2.
 echo "Running example class $1"
 
 # Run spark shell locally
-${SPARK_HOME}/bin/spark-submit   --conf "spark.executor.memory=512m" \
-				--conf "spark.driver.memory=512m" \
+${SPARK_HOME}/bin/spark-submit   --conf "spark.executor.memory=1g" \
+				--conf "spark.driver.memory=1g" \
 				--conf "spark.rdd.compress=true" \
 				--conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
 				--conf "spark.kryoserializer.buffer=24mb" \
