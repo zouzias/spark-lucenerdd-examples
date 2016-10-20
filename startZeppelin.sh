791956f28dd1c54b4a9a29084b00b1903d3cbfc8
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2016-10-20 10:52:05 +0200

    scripts: add comments

--- startZeppelin.sh
+++ startZeppelin.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#############################################################
+# Start Zeppelin using Docker                               #
+#############################################################
+# Note: To run this, you need:                              #
+# (1) Docker installed, i.e., docker-machine is available   #
+# (2) There is a "default" docker machine available         #
+#############################################################
+
 DOCKER_NAME="default"
 docker-machine start ${DOCKER_NAME}
 DOCKER_MACHINE_IP=`docker-machine ip ${DOCKER_NAME}`
